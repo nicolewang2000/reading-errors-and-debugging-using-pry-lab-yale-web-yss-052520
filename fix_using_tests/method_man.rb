c389f6950b063abfc9a25733f1479130dfd3a9a8
Nicole Wang <nicole.wang@yale.edu>
2020-05-09 15:07:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
 
-def start_game(player1 = "Rocky", player2 = "Bullwinkle")
-  "Hello #{player1} & #{playar2}"
+def start_game(player1, player2)
+  return "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player1, player2)
